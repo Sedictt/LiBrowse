333b1750e913d2c01058f097ceb40d8e35ee7abb
Jv <jvtillo10@gmail.com>
2025-10-01 20:48:37 +0800

    Clean up main branch: Keep only project structure

--- database/migrate_add_borrow_fields.sql
+++
@@ -1,6 +0,0 @@
--- Migration script to add new borrow request fields
--- Run this if you have an existing database
-
-ALTER TABLE transactions 
-ADD COLUMN preferred_pickup_time DATETIME NULL COMMENT 'Borrower preferred pickup time' AFTER pickup_location,
-ADD COLUMN borrow_duration ENUM('1-week', '2-weeks', '3-weeks', '1-month', 'custom') NULL COMMENT 'How long they want to borrow' AFTER preferred_pickup_time;
